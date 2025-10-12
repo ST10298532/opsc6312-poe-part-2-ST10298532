59fd6b72cd21657df133390c53940169cf89f3f3
jaydenh <st10298532@imconnect.edu.za>
2025-10-12 18:05:11 +0200

    Added Save/Unsave event functionality and updated adapter behavior

--- app/build/tmp/kapt3/stubs/debug/com/example/eventstrack/api/EventRequest.java
+++ app/build/tmp/kapt3/stubs/debug/com/example/eventstrack/api/EventRequest.java
@@ -1,41 +1,75 @@
 package com.example.eventstrack.api;
 
-@kotlin.Metadata(mv = {2, 2, 0}, k = 1, xi = 48, d1 = {"\u0000\"\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0000\n\u0002\u0010\u000e\n\u0002\b\u0010\n\u0002\u0010\u000b\n\u0002\b\u0002\n\u0002\u0010\b\n\u0002\b\u0002\b\u0086\b\u0018\u00002\u00020\u0001B\'\u0012\u0006\u0010\u0002\u001a\u00020\u0003\u0012\u0006\u0010\u0004\u001a\u00020\u0003\u0012\u0006\u0010\u0005\u001a\u00020\u0003\u0012\u0006\u0010\u0006\u001a\u00020\u0003\u00a2\u0006\u0004\b\u0007\u0010\bJ\t\u0010\u000e\u001a\u00020\u0003H\u00c6\u0003J\t\u0010\u000f\u001a\u00020\u0003H\u00c6\u0003J\t\u0010\u0010\u001a\u00020\u0003H\u00c6\u0003J\t\u0010\u0011\u001a\u00020\u0003H\u00c6\u0003J1\u0010\u0012\u001a\u00020\u00002\b\b\u0002\u0010\u0002\u001a\u00020\u00032\b\b\u0002\u0010\u0004\u001a\u00020\u00032\b\b\u0002\u0010\u0005\u001a\u00020\u00032\b\b\u0002\u0010\u0006\u001a\u00020\u0003H\u00c6\u0001J\u0013\u0010\u0013\u001a\u00020\u00142\b\u0010\u0015\u001a\u0004\u0018\u00010\u0001H\u00d6\u0003J\t\u0010\u0016\u001a\u00020\u0017H\u00d6\u0001J\t\u0010\u0018\u001a\u00020\u0003H\u00d6\u0001R\u0011\u0010\u0002\u001a\u00020\u0003\u00a2\u0006\b\n\u0000\u001a\u0004\b\t\u0010\nR\u0011\u0010\u0004\u001a\u00020\u0003\u00a2\u0006\b\n\u0000\u001a\u0004\b\u000b\u0010\nR\u0011\u0010\u0005\u001a\u00020\u0003\u00a2\u0006\b\n\u0000\u001a\u0004\b\f\u0010\nR\u0011\u0010\u0006\u001a\u00020\u0003\u00a2\u0006\b\n\u0000\u001a\u0004\b\r\u0010\n\u00a8\u0006\u0019"}, d2 = {"Lcom/example/eventstrack/api/EventRequest;", "", "name", "", "date", "location", "description", "<init>", "(Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;)V", "getName", "()Ljava/lang/String;", "getDate", "getLocation", "getDescription", "component1", "component2", "component3", "component4", "copy", "equals", "", "other", "hashCode", "", "toString", "app_debug"})
+@kotlin.Metadata(mv = {2, 2, 0}, k = 1, xi = 48, d1 = {"\u0000*\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0000\n\u0002\u0010\u000e\n\u0002\b\u0006\n\u0002\u0010\u0006\n\u0002\b\u001a\n\u0002\u0010\u000b\n\u0002\b\u0002\n\u0002\u0010\b\n\u0002\b\u0002\b\u0086\b\u0018\u00002\u00020\u0001BW\u0012\u0006\u0010\u0002\u001a\u00020\u0003\u0012\u0006\u0010\u0004\u001a\u00020\u0003\u0012\b\b\u0002\u0010\u0005\u001a\u00020\u0003\u0012\u0006\u0010\u0006\u001a\u00020\u0003\u0012\u0006\u0010\u0007\u001a\u00020\u0003\u0012\u0006\u0010\b\u001a\u00020\u0003\u0012\b\b\u0002\u0010\t\u001a\u00020\n\u0012\b\b\u0002\u0010\u000b\u001a\u00020\n\u0012\b\b\u0002\u0010\f\u001a\u00020\u0003\u00a2\u0006\u0004\b\r\u0010\u000eJ\t\u0010\u001a\u001a\u00020\u0003H\u00c6\u0003J\t\u0010\u001b\u001a\u00020\u0003H\u00c6\u0003J\t\u0010\u001c\u001a\u00020\u0003H\u00c6\u0003J\t\u0010\u001d\u001a\u00020\u0003H\u00c6\u0003J\t\u0010\u001e\u001a\u00020\u0003H\u00c6\u0003J\t\u0010\u001f\u001a\u00020\u0003H\u00c6\u0003J\t\u0010 \u001a\u00020\nH\u00c6\u0003J\t\u0010!\u001a\u00020\nH\u00c6\u0003J\t\u0010\"\u001a\u00020\u0003H\u00c6\u0003Jc\u0010#\u001a\u00020\u00002\b\b\u0002\u0010\u0002\u001a\u00020\u00032\b\b\u0002\u0010\u0004\u001a\u00020\u00032\b\b\u0002\u0010\u0005\u001a\u00020\u00032\b\b\u0002\u0010\u0006\u001a\u00020\u00032\b\b\u0002\u0010\u0007\u001a\u00020\u00032\b\b\u0002\u0010\b\u001a\u00020\u00032\b\b\u0002\u0010\t\u001a\u00020\n2\b\b\u0002\u0010\u000b\u001a\u00020\n2\b\b\u0002\u0010\f\u001a\u00020\u0003H\u00c6\u0001J\u0013\u0010$\u001a\u00020%2\b\u0010&\u001a\u0004\u0018\u00010\u0001H\u00d6\u0003J\t\u0010\'\u001a\u00020(H\u00d6\u0001J\t\u0010)\u001a\u00020\u0003H\u00d6\u0001R\u0011\u0010\u0002\u001a\u00020\u0003\u00a2\u0006\b\n\u0000\u001a\u0004\b\u000f\u0010\u0010R\u0011\u0010\u0004\u001a\u00020\u0003\u00a2\u0006\b\n\u0000\u001a\u0004\b\u0011\u0010\u0010R\u0011\u0010\u0005\u001a\u00020\u0003\u00a2\u0006\b\n\u0000\u001a\u0004\b\u0012\u0010\u0010R\u0011\u0010\u0006\u001a\u00020\u0003\u00a2\u0006\b\n\u0000\u001a\u0004\b\u0013\u0010\u0010R\u0011\u0010\u0007\u001a\u00020\u0003\u00a2\u0006\b\n\u0000\u001a\u0004\b\u0014\u0010\u0010R\u0011\u0010\b\u001a\u00020\u0003\u00a2\u0006\b\n\u0000\u001a\u0004\b\u0015\u0010\u0010R\u0011\u0010\t\u001a\u00020\n\u00a2\u0006\b\n\u0000\u001a\u0004\b\u0016\u0010\u0017R\u0011\u0010\u000b\u001a\u00020\n\u00a2\u0006\b\n\u0000\u001a\u0004\b\u0018\u0010\u0017R\u0011\u0010\f\u001a\u00020\u0003\u00a2\u0006\b\n\u0000\u001a\u0004\b\u0019\u0010\u0010\u00a8\u0006*"}, d2 = {"Lcom/example/eventstrack/api/EventRequest;", "", "title", "", "description", "category", "start_utc", "end_utc", "venue_name", "lat", "", "lng", "image_url", "<init>", "(Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;DDLjava/lang/String;)V", "getTitle", "()Ljava/lang/String;", "getDescription", "getCategory", "getStart_utc", "getEnd_utc", "getVenue_name", "getLat", "()D", "getLng", "getImage_url", "component1", "component2", "component3", "component4", "component5", "component6", "component7", "component8", "component9", "copy", "equals", "", "other", "hashCode", "", "toString", "app_debug"})
 public final class EventRequest {
     @org.jetbrains.annotations.NotNull()
-    private final java.lang.String name = null;
-    @org.jetbrains.annotations.NotNull()
-    private final java.lang.String date = null;
-    @org.jetbrains.annotations.NotNull()
-    private final java.lang.String location = null;
+    private final java.lang.String title = null;
     @org.jetbrains.annotations.NotNull()
     private final java.lang.String description = null;
+    @org.jetbrains.annotations.NotNull()
+    private final java.lang.String category = null;
+    @org.jetbrains.annotations.NotNull()
+    private final java.lang.String start_utc = null;
+    @org.jetbrains.annotations.NotNull()
+    private final java.lang.String end_utc = null;
+    @org.jetbrains.annotations.NotNull()
+    private final java.lang.String venue_name = null;
+    private final double lat = 0.0;
+    private final double lng = 0.0;
+    @org.jetbrains.annotations.NotNull()
+    private final java.lang.String image_url = null;
     
     public EventRequest(@org.jetbrains.annotations.NotNull()
-    java.lang.String name, @org.jetbrains.annotations.NotNull()
-    java.lang.String date, @org.jetbrains.annotations.NotNull()
-    java.lang.String location, @org.jetbrains.annotations.NotNull()
-    java.lang.String description) {
+    java.lang.String title, @org.jetbrains.annotations.NotNull()
+    java.lang.String description, @org.jetbrains.annotations.NotNull()
+    java.lang.String category, @org.jetbrains.annotations.NotNull()
+    java.lang.String start_utc, @org.jetbrains.annotations.NotNull()
+    java.lang.String end_utc, @org.jetbrains.annotations.NotNull()
+    java.lang.String venue_name, double lat, double lng, @org.jetbrains.annotations.NotNull()
+    java.lang.String image_url) {
         super();
     }
     
     @org.jetbrains.annotations.NotNull()
-    public final java.lang.String getName() {
-        return null;
-    }
-    
-    @org.jetbrains.annotations.NotNull()
-    public final java.lang.String getDate() {
-        return null;
-    }
-    
-    @org.jetbrains.annotations.NotNull()
-    public final java.lang.String getLocation() {
+    public final java.lang.String getTitle() {
         return null;
     }
     
     @org.jetbrains.annotations.NotNull()
     public final java.lang.String getDescription() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String getCategory() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String getStart_utc() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String getEnd_utc() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String getVenue_name() {
+        return null;
+    }
+    
+    public final double getLat() {
+        return 0.0;
+    }
+    
+    public final double getLng() {
+        return 0.0;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String getImage_url() {
         return null;
     }
     
@@ -60,11 +94,37 @@ public final class EventRequest {
     }
     
     @org.jetbrains.annotations.NotNull()
+    public final java.lang.String component5() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String component6() {
+        return null;
+    }
+    
+    public final double component7() {
+        return 0.0;
+    }
+    
+    public final double component8() {
+        return 0.0;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String component9() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
     public final com.example.eventstrack.api.EventRequest copy(@org.jetbrains.annotations.NotNull()
-    java.lang.String name, @org.jetbrains.annotations.NotNull()
-    java.lang.String date, @org.jetbrains.annotations.NotNull()
-    java.lang.String location, @org.jetbrains.annotations.NotNull()
-    java.lang.String description) {
+    java.lang.String title, @org.jetbrains.annotations.NotNull()
+    java.lang.String description, @org.jetbrains.annotations.NotNull()
+    java.lang.String category, @org.jetbrains.annotations.NotNull()
+    java.lang.String start_utc, @org.jetbrains.annotations.NotNull()
+    java.lang.String end_utc, @org.jetbrains.annotations.NotNull()
+    java.lang.String venue_name, double lat, double lng, @org.jetbrains.annotations.NotNull()
+    java.lang.String image_url) {
         return null;
     }
     
