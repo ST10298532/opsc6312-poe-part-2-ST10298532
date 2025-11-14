2b40fa3f14bd1e0ca00d687b86410d5bc6283374
jaydenh <st10298532@imconnect.edu.za>
2025-11-14 07:44:46 +0200

    Saving current work before creating part3 branch

--- app/build/tmp/kapt3/stubs/debug/com/example/eventstrack/api/ApiClient.java
+++ app/build/tmp/kapt3/stubs/debug/com/example/eventstrack/api/ApiClient.java
@@ -3,7 +3,7 @@ package com.example.eventstrack.api;
 @kotlin.Metadata(mv = {2, 2, 0}, k = 1, xi = 48, d1 = {"\u0000\u001a\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0002\b\u0003\n\u0002\u0010\u000e\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0005\b\u00c6\u0002\u0018\u00002\u00020\u0001B\t\b\u0002\u00a2\u0006\u0004\b\u0002\u0010\u0003R\u000e\u0010\u0004\u001a\u00020\u0005X\u0082T\u00a2\u0006\u0002\n\u0000R\u001b\u0010\u0006\u001a\u00020\u00078FX\u0086\u0084\u0002\u00a2\u0006\f\n\u0004\b\n\u0010\u000b\u001a\u0004\b\b\u0010\t\u00a8\u0006\f"}, d2 = {"Lcom/example/eventstrack/api/ApiClient;", "", "<init>", "()V", "BASE_URL", "", "instance", "Lretrofit2/Retrofit;", "getInstance", "()Lretrofit2/Retrofit;", "instance$delegate", "Lkotlin/Lazy;", "app_debug"})
 public final class ApiClient {
     @org.jetbrains.annotations.NotNull()
-    private static final java.lang.String BASE_URL = "http://10.0.2.2:4000/";
+    private static final java.lang.String BASE_URL = "http://192.168.88.231:4000/";
     @org.jetbrains.annotations.NotNull()
     private static final kotlin.Lazy instance$delegate = null;
     @org.jetbrains.annotations.NotNull()
